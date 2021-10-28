542b95dc1abb2c1c41e169ae728de8ea059c15dd
Ross Philipson <ross.philipson@oracle.com>
2021-10-28 16:04:46 -0400

    IVRS decoding and tracing
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- iommu-util/ivrs.c
+++ iommu-util/ivrs.c
@@ -41,6 +41,180 @@
 #include <unistd.h>
 #include "defs.h"
 
+#define IVRS_REV_FIXED	0x1
+#define IVRS_REV_MIXED	0x2
+
+struct acpi_table_ivrs {
+	struct acpi_table_header header;	/* Common ACPI table header */
+	uint32_t ivinfo;			/* IVRS IVinfo Field */
+	uint8_t reserved[8];
+	/* IVDBs */
+} __attribute__((packed));
+
+struct ivrs_ivinfo {
+	union {
+		uint32_t val;
+		struct {
+			uint32_t efr_sup:1;
+			uint32_t dma_remap_sup:1;
+			uint32_t reserved1:3;
+			uint32_t gva_size:3;
+			uint32_t pa_size:7;
+			uint32_t va_size:7;
+			uint32_t ht_ats_resv:1;
+			uint32_t reserved2:9;
+		};
+	};
+} __attribute__((packed));
+
+struct ivrs_ivdb_common {
+	uint8_t type;
+	uint8_t flags;
+	uint16_t length;
+} __attribute__((packed));
+
+#define IVHD_TYPE_10H	0x10
+#define IVHD_TYPE_11H	0x11
+#define IVHD_TYPE_40H	0x40
+
+struct ivrs_ivhd_header {
+	struct ivrs_ivdb_common ivdb;
+	uint16_t devid;
+	uint16_t cap_offset;
+	uint64_t base_addr;	/* MMIO Address/BAR */
+	uint16_t pci_seg;
+	uint16_t iommu_info;
+	uint32_t feature_info;
+	/* Type 10: device entries */
+	/* Type 11/40: ivrs_ivhd_11_40 ... device entries */
+} __attribute__((packed));
+
+struct ivrs_ivhd_11_40 {
+	uint64_t efr_info;	/* Types 11h/40h only */
+	uint64_t reserved;
+} __attribute__((packed));
+
+#define IVMD_TYPE_ALL_PERIPHERALS	0x20
+#define IVMD_TYPE_SPECIFIED_PERIPHERAL	0x21
+#define IVMD_TYPE_PERIPHERALS_RANGE	0x22
+
+struct ivrs_ivmd_header {
+	struct ivrs_ivdb_common ivdb;
+	uint16_t devid;
+	uint16_t aux_data;
+	uint64_t reserved;
+	uint64_t start_addr;
+	uint64_t mem_block_length;
+} __attribute__((packed));
+
+struct ivmd_flags {
+	union {
+		uint8_t val;
+		struct {
+			uint8_t unity:1;
+			uint8_t ir:1;
+			uint8_t iw:1;
+			uint8_t excl_range:1;
+			uint8_t reserved:4;
+		};
+	};
+} __attribute__((packed));
+
+static void
+acpi_parse_one_ivhd(struct ivrs_ivdb_common *ivdb)
+{
+}
+
+static void
+acpi_parse_one_ivmd(struct ivrs_ivdb_common *ivdb)
+{
+}
+
+static void
+acpi_parse_ivrs(struct acpi_table_header *table)
+{
+	struct acpi_table_ivrs *ivrs;
+	struct ivrs_ivinfo info;
+	struct ivrs_ivdb_common *ivdb;
+	uint8_t *p;
+	int i = 1, j = 1, k = 1;
+
+	ivrs = (struct acpi_table_ivrs *)table;
+
+	printf("I/O Virtualization Reporting Structure\n");
+	printf("==================================================\n");
+	printf("Signature:        %.4s\n", ivrs->header.signature);
+	printf("Length:           0x%8.8x\n", ivrs->header.length);
+	printf("Revision:         0x%2.2x\n", ivrs->header.revision);
+	printf("Checksum:         0x%2.2x\n", ivrs->header.checksum);
+	printf("OEMID:            %.*s\n", ACPI_OEM_ID_SIZE, ivrs->header.oem_id);
+	printf("OEM Table ID:     %.*s\n", ACPI_OEM_TABLE_ID_SIZE, ivrs->header.oem_table_id);
+	printf("OEM Revision:     0x%8.8x\n", ivrs->header.oem_revision);
+	printf("Creator ID:       %.*s\n", ACPI_NAME_SIZE, ivrs->header.asl_compiler_id);
+	printf("Creator Revision: 0x%8.8x\n", ivrs->header.asl_compiler_revision);
+	printf("IVInfo:           0x%8.8x\n", ivrs->ivinfo);
+
+	p = &ivrs->reserved[0];
+	printf("Reserved[10]: %2.2x %2.2x %2.2x %2.2x %2.2x %2.2x %2.2x %2.2x\n",
+	  p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]);
+
+	info.val = ivrs->ivinfo;
+	printf("  IVRS Info Field\n");
+	printf("  ------------------------------------------------\n");
+	printf("   EFRSup:          0x%1.1x\n", info.efr_sup);
+	printf("   DMA Remap Sup:   0x%1.1x\n", info.dma_remap_sup);
+	printf("   Reserved:        0x%3.3x\n", info.reserved1);
+	printf("   GVA Size:        0x%3.3x\n", info.gva_size);
+	printf("   PA Size:         0x%7.7x\n", info.pa_size);
+	printf("   VA Size:         0x%7.7x\n", info.va_size);
+	printf("   HtAtsResv:       0x%1.1x\n", info.ht_ats_resv);
+	printf("   Reserved:        0x%9.9x\n", info.reserved2);
+	printf("  ------------------------------------------------\n");
+
+	ivdb = (struct ivrs_ivdb_common *)(ivrs + 1);
+
+	printf("\nIVDB Structures...\n\n");
+
+	while ( ((unsigned long)ivdb) <
+		(((unsigned long)ivrs) + table->length) ) {
+		switch ( ivdb->type ) {
+		case IVHD_TYPE_10H:
+		case IVHD_TYPE_11H:
+		case IVHD_TYPE_40H:
+			printf("I/O Virtualization Hardware Definition (IVHD) Structure #%d\n", i++);
+			printf("Type:           0x%2.2x\n", ivdb->type);
+			printf("Flags:          0x%2.2x\n", ivdb->flags);
+			printf("Length:         0x%4.4x\n", ivdb->length);
+			acpi_parse_one_ivhd(ivdb);
+			printf("\n");
+			break;
+		case IVMD_TYPE_ALL_PERIPHERALS:
+		case IVMD_TYPE_SPECIFIED_PERIPHERAL:
+		case IVMD_TYPE_PERIPHERALS_RANGE:
+			printf("I/O Virtualization Memory Definition (IVMD) Structure #%d\n", j++);
+			printf("Type:           0x%2.2x\n", ivdb->type);
+			printf("Flags:          0x%2.2x\n", ivdb->flags);
+			printf("Length:         0x%4.4x\n", ivdb->length);
+			acpi_parse_one_ivmd(ivdb);
+			printf("\n");
+			break;
+		default:
+			printf("Unknown Reporting Structure #%d\n", k++);
+			printf("Type:           0x%2.2x\n", ivdb->type);
+			printf("Flags:          0x%2.2x\n", ivdb->flags);
+			printf("Length:         0x%4.4x\n", ivdb->length);
+			printf("\n");
+			break;
+		}
+
+		ivdb = ((void *)ivdb + ivdb->length);
+
+	}
+
+	printf("==================================================\n");
+	printf("End IVRS\n");
+}
+
 void
 decode_ivrs_table(void)
 {
@@ -62,7 +236,7 @@ decode_ivrs_table(void)
 		goto done;
 	}
 
-	/* TODO acpi_parse_dmar(table);*/
+	acpi_parse_ivrs(table);
 
 done:
 	if (table != NULL)
@@ -103,7 +277,7 @@ decode_ivrs_table_file(const char *file)
 		goto done;
 	}
 
-	/* TOOD acpi_parse_ivrs(table);*/
+	acpi_parse_ivrs(table);
 
 done:
 	if (table != NULL)
